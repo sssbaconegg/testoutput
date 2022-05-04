b59bd6404902303d924c18d3cf139de15440aa4f
sssbaconegg <100740633+sssbaconegg@users.noreply.github.com>
2022-05-04 10:50:06 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True ,
+  name='testoutputage',
+  version = '1,0,0',
+  description = 'oss- development class test output',
+  author = 'baconegg',
+  author_email = 'baconegg@donga.ac.kr',
+  url='https://github.com/sssbaconegg/testoutput/;,
+  install_requires= ['pytest'],
+  long_description = 'oss development ...',
+  long_description_content_type = 'text/markdown',
+  classifiers = [
+    "programming language :: Python :: 3",
+    "operating system :: os independent",
+  ],
+)
